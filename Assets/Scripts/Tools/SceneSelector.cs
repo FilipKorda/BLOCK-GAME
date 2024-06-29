57f253734c2780a67688d3e5763ee5470c670159
FilipKorda <filipkorda00@gmail.com>
2024-06-29 13:04:47 +0200

    addd editor script

---
+++ Assets/Scripts/Tools/SceneSelector.cs
@@ -0,0 +1,44 @@
+using System.Linq;
+using UnityEditor;
+using UnityEditor.SceneManagement;
+using UnityEngine;
+
+public class SceneSelector : EditorWindow
+{
+    void OnGUI()
+    {
+        GUILayout.BeginHorizontal();
+        GUILayout.FlexibleSpace();
+        GUILayout.Label("--== Build Settings Scenes ==--", EditorStyles.boldLabel);
+        GUILayout.FlexibleSpace();
+        GUILayout.EndHorizontal();
+
+        string[] scenes = EditorBuildSettings.scenes.Select(s => System.IO.Path.GetFileNameWithoutExtension(s.path)).ToArray();
+
+        foreach (string scene in scenes)
+        {
+            if (GUILayout.Button(scene))
+            {
+                SwitchToScene(scene);
+            }
+        }
+    }
+
+    void SwitchToScene(string sceneName)
+    {
+        if (EditorSceneManager.SaveCurrentModifiedScenesIfUserWantsTo())
+        {
+            string scenePath = EditorBuildSettings.scenes.FirstOrDefault(s => System.IO.Path.GetFileNameWithoutExtension(s.path) == sceneName)?.path;
+            if (!string.IsNullOrEmpty(scenePath))
+            {
+                EditorSceneManager.OpenScene(scenePath);
+            }
+        }
+    }
+
+    [MenuItem("Tools/Custom Build Settings Window")]
+    public static void ShowWindow()
+    {
+        EditorWindow.GetWindow(typeof(SceneSelector), false, "Build Settings");
+    }
+}
