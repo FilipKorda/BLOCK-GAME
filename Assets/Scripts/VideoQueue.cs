13ff3f163babd337eb889022f8a7bb12ce8b1b16
FilipKorda <filipkorda00@gmail.com>
2024-07-06 19:51:01 +0200

    Update

---
+++ Assets/Scripts/VideoQueue.cs
@@ -0,0 +1,72 @@
+using TMPro;
+using UnityEngine;
+using UnityEngine.Video;
+
+public class VideoQueue : MonoBehaviour
+{
+    public VideoPlayer videoPlayer;
+    public VideoClip[] videoClips;
+    public TextMeshProUGUI numberText;
+    private int currentVideoIndex = 0;
+
+    void Start()
+    {
+        UpdateNumberText();
+
+        if (videoClips.Length > 0)
+        {
+            videoPlayer.clip = videoClips[currentVideoIndex];
+            videoPlayer.Play();
+        }
+        videoPlayer.loopPointReached += OnVideoEnd;
+    }
+
+    void Update()
+    {
+        if (Input.GetKeyDown(KeyCode.Q))
+        {
+            PlayPreviousVideo();
+        }
+        else if (Input.GetKeyDown(KeyCode.E))
+        {
+            PlayNextVideo();
+        }
+    }
+
+    void PlayPreviousVideo()
+    {
+        currentVideoIndex--;
+        if (currentVideoIndex < 0)
+        {
+            currentVideoIndex = videoClips.Length - 1;
+        }
+        PlayVideoAtIndex(currentVideoIndex);
+    }
+
+    void PlayNextVideo()
+    {
+        currentVideoIndex++;
+        if (currentVideoIndex >= videoClips.Length)
+        {
+            currentVideoIndex = 0;
+        }
+        PlayVideoAtIndex(currentVideoIndex);
+    }
+
+    void PlayVideoAtIndex(int index)
+    {
+        videoPlayer.clip = videoClips[index];
+        videoPlayer.Play();
+        UpdateNumberText();
+    }
+
+    void OnVideoEnd(VideoPlayer vp)
+    {
+        PlayNextVideo();
+    }
+
+    void UpdateNumberText()
+    {
+        numberText.text = (currentVideoIndex + 1).ToString();
+    }
+}
