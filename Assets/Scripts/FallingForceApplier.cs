6145ae69b53ecc5107853a1a0d3b2d6f27d16e5d
FilipKorda <filipkorda00@gmail.com>
2024-07-20 21:14:27 +0200

    Update

---
+++ Assets/Scripts/FallingForceApplier.cs
@@ -0,0 +1,63 @@
+using UnityEngine;
+
+public class FallingForceApplier : MonoBehaviour
+{
+    public enum DirectionForceApplier
+    {
+        None,
+        W,
+        S,
+        A,
+        D
+    }
+
+    [Header("Force Directions")]
+    [SerializeField] private DirectionForceApplier[] forceDirections;
+    [SerializeField] private float forceStrength = 35f;
+    [SerializeField] private Player player;
+    [SerializeField] private GameObject collidersGameObject;
+
+
+    private Vector3 forceDirection_W = new(0, -20, 5);
+    private Vector3 forceDirection_A = new(-5, -20, 0);
+    private Vector3 forceDirection_S = new(0, -20, -5);
+    private Vector3 forceDirection_D = new(5, -20, 0);
+
+    private void OnTriggerEnter(Collider other)
+    {
+        if (other.TryGetComponent<Rigidbody>(out var rb))
+        {
+            player.PreparPlayerToFallDown();
+            Vector3 force = Vector3.zero;
+
+            foreach (var direction in forceDirections)
+            {
+                if (direction == player.rotationDirection)
+                {
+                    switch (direction)
+                    {
+                        case DirectionForceApplier.W:
+                            force += new Vector3(0, -forceStrength, 5);
+                            break;
+                        case DirectionForceApplier.S:
+                            force += new Vector3(0, -forceStrength, -5);
+                            break;
+                        case DirectionForceApplier.A:
+                            force += new Vector3(-5, -forceStrength, 0);
+                            break;
+                        case DirectionForceApplier.D:
+                            force += new Vector3(5, -forceStrength, 0);
+                            break;
+                        default:
+                            break;
+                    }
+                }
+            }
+
+
+            collidersGameObject.SetActive(false);
+
+            rb.AddForce(force, ForceMode.Impulse);
+        }
+    }
+}
--- Assets/Scripts/Player.cs
+++ Assets/Scripts/Player.cs
@@ -2,21 +2,16 @@ using UnityEngine;
 
 public class Player : MonoBehaviour
 {
+    public FallingForceApplier.DirectionForceApplier rotationDirection;
     private readonly float rotationSpeed = 750f;
     private float totalRotation;
     private bool isRotating;
-    private Direction rotationDirection;
+
     public Vector3 pivot, axis, scale;
     private Rigidbody rb;
 
     [Header("Player Management")]
     public bool canMove;
-
-    [Header("Force Directions")]
-    [SerializeField] private Vector3 forceDirection_W = new(0, -10, 10);
-    [SerializeField] private Vector3 forceDirection_A = new(-10, -10, 0);
-    [SerializeField] private Vector3 forceDirection_S = new(0, -10, -10);
-    [SerializeField] private Vector3 forceDirection_D = new(10, -10, 0);
 
     [Header("Layer Mask of Invisible Wall")]
     [SerializeField] private LayerMask collisionLayerMask;
@@ -47,20 +42,19 @@ public class Player : MonoBehaviour
                     moveTracker.AddMove();
                     DetectSurface();
                 }
-                if ((rotationDirection == Direction.A) || (rotationDirection == Direction.W))
+                if ((rotationDirection == FallingForceApplier.DirectionForceApplier.A) || (rotationDirection == FallingForceApplier.DirectionForceApplier.W))
                     transform.RotateAround(pivot, axis, deltaRotation);
                 else transform.RotateAround(pivot, axis, -deltaRotation);
                 totalRotation += deltaRotation;
             }
-            else if (Input.GetKeyDown(KeyCode.W)) Rotate(Direction.W);
-            else if (Input.GetKeyDown(KeyCode.A)) Rotate(Direction.A);
-            else if (Input.GetKeyDown(KeyCode.S)) Rotate(Direction.S);
-            else if (Input.GetKeyDown(KeyCode.D)) Rotate(Direction.D);
+            else if (Input.GetKeyDown(KeyCode.W)) Rotate(FallingForceApplier.DirectionForceApplier.W);
+            else if (Input.GetKeyDown(KeyCode.A)) Rotate(FallingForceApplier.DirectionForceApplier.A);
+            else if (Input.GetKeyDown(KeyCode.S)) Rotate(FallingForceApplier.DirectionForceApplier.S);
+            else if (Input.GetKeyDown(KeyCode.D)) Rotate(FallingForceApplier.DirectionForceApplier.D);
         }
-
     }
 
-    void Rotate(Direction direction)
+    void Rotate(FallingForceApplier.DirectionForceApplier direction)
     {
         rotationDirection = direction;
         isRotating = true;
@@ -68,20 +62,20 @@ public class Player : MonoBehaviour
 
         switch (rotationDirection)
         {
-            case Direction.D:
+            case FallingForceApplier.DirectionForceApplier.D:
                 pivot = transform.position + new Vector3(scale.x, -scale.y, 0);
                 break;
-            case Direction.A:
+            case FallingForceApplier.DirectionForceApplier.A:
                 pivot = transform.position + new Vector3(-scale.x, -scale.y, 0);
                 break;
-            case Direction.W:
+            case FallingForceApplier.DirectionForceApplier.W:
                 pivot = transform.position + new Vector3(0, -scale.y, scale.z);
                 break;
-            case Direction.S:
+            case FallingForceApplier.DirectionForceApplier.S:
                 pivot = transform.position + new Vector3(0, -scale.y, -scale.z);
                 break;
         }
-        if ((rotationDirection == Direction.D) || (rotationDirection == Direction.A))
+        if ((rotationDirection == FallingForceApplier.DirectionForceApplier.D) || (rotationDirection == FallingForceApplier.DirectionForceApplier.A))
         {
             axis = Vector3.forward;
             (scale.y, scale.x) = (scale.x, scale.y);
@@ -104,71 +98,16 @@ public class Player : MonoBehaviour
         }
     }
 
-
-    void OnCollisionEnter(Collision collision)
-    {
-        if ((collisionLayerMask.value & (1 << collision.gameObject.layer)) == 0)
-        {
-            return;
-        }
-
-        Vector3 collisionDirection = (collision.transform.position - transform.position).normalized;
-        Debug.Log("Collision Direction: " + collisionDirection);
-        Debug.Log("Collision Direction: " + collision.gameObject.name);
-        Destroy(collision.gameObject);
-
-        SoundManager.Instance.PlaySound(SoundClip.LoseSound);
-
-        if (IsInRange(collisionDirection, new Vector3(-1f, -1f, 0f), new Vector3(1f, 0f, 1f)))  // to dzia³a na przycisk W
-        {
-            canMove = false;
-            UnfreezeRotation();
-            rb.AddForce(forceDirection_W, ForceMode.Impulse);
-        }
-        if (IsInRange(collisionDirection, new Vector3(-1f, -1f, -1f), new Vector3(0f, 0f, 1f)))// to dzia³a na przycisk A
-        {
-            canMove = false;
-            UnfreezeRotation();
-            rb.AddForce(forceDirection_A, ForceMode.Impulse);
-        }
-        if (IsInRange(collisionDirection, new Vector3(-1f, -1f, -1f), new Vector3(1f, 0f, 0f))) // to dzia³a na przycisk S
-        {
-            canMove = false;
-            UnfreezeRotation();
-            rb.AddForce(forceDirection_S, ForceMode.Impulse);
-        }
-        if (IsInRange(collisionDirection, new Vector3(0f, -1f, 0f), new Vector3(1f, 0f, 0f))) // to dzia³a na przycisk D
-        {
-            canMove = false;
-            UnfreezeRotation();
-            rb.AddForce(forceDirection_D, ForceMode.Impulse);
-        }
-    }
-
     void FreezeAllAxes()
     {
         rb.constraints = RigidbodyConstraints.FreezeAll;
     }
 
-    void UnfreezeRotation()
+    public void PreparPlayerToFallDown()
     {
         rb.constraints = RigidbodyConstraints.None;
+        rb.useGravity = true;
+        canMove = false;
     }
-
-    bool IsInRange(Vector3 value, Vector3 min, Vector3 max)
-    {
-        return value.x >= min.x && value.x <= max.x &&
-               value.y >= min.y && value.y <= max.y &&
-               value.z >= min.z && value.z <= max.z;
-    }
-
 }
 
-public enum Direction
-{
-    None,
-    W,
-    S,
-    D,
-    A
-}
